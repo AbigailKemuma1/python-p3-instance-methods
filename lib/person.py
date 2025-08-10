9afec41d9aa5420355fdf727287cb19f10fd13d1
AbigailKemuma1 <abigail.kemuma@student.moringaschool.com>
2025-08-10 13:36:55 +0300

    Adds instance methods

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+class Person:
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+if __name__ == "__main__":
+    alice = Person()
+    alice.talk()
+    alice.walk()
